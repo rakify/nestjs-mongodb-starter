43840d601ae23d91029abff6bd50db5aab075aa1
rakiblh <rakib@lemonhivee.com>
2023-06-21 17:30:19 +0600

    chore: cleaning

--- src/core/environments/index.ts
+++ src/core/environments/index.ts
@@ -1,7 +1,6 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const DB_DATABASE = process.env.DB_DATABASE;
 const DB_CONNECTION = process.env.DB_CONNECTION;
 
 const MODE = process.env.MODE;
@@ -9,11 +8,4 @@ const PORT = process.env.PORT;
 const JWT_SECRET = process.env.JWT_SECRET;
 const DEFAULT_ADMIN_EMAIL = process.env.DEFAULT_ADMIN_EMAIL;
 
-export {
-  DB_CONNECTION,
-  DB_DATABASE,
-  MODE,
-  PORT,
-  JWT_SECRET,
-  DEFAULT_ADMIN_EMAIL,
-};
+export { DB_CONNECTION, MODE, PORT, JWT_SECRET, DEFAULT_ADMIN_EMAIL };
