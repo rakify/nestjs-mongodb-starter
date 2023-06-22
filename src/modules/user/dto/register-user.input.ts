4e4874c2d7d1769b86c0ab121d3d5c0292fbb948
rakiblh <rakib@lemonhivee.com>
2023-06-22 13:51:53 +0600

    fix: during register it should always validate

--- src/modules/user/dto/register-user.input.ts
+++ src/modules/user/dto/register-user.input.ts
@@ -1,21 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql';
-import {
-  IsEmail,
-  IsString,
-  MaxLength,
-  MinLength,
-  ValidateIf,
-} from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class RegisterUserInput {
   @Field({ nullable: false, description: 'user input value for Email' })
-  @ValidateIf((_, value) => value !== undefined && value !== null)
   @IsEmail()
   email: string;
 
   @Field({ nullable: false, description: 'user input value for Password' })
-  @ValidateIf((_, value) => value !== undefined && value !== null)
   @IsString()
   @MinLength(4, {
     message: 'Password is too short',
